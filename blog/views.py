2bed5e83bd08eda77ffcfa9d4f4b0a6d418be40c
Jeyson Miranda <jeysonmiranda64@gmail.com>
2025-10-05 20:13:28 +0530

    Initial commit - Django blog project

---
+++ blog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+import logging
+from .models import Post
+
+# Create your views here.
+# static demo data
+# posts = [
+#         {'id':1, 'title': 'post 1', 'content': 'content of post 1'},
+#         {'id':2, 'title': 'post 2', 'content': 'content of post 2'},
+#         {'id':3, 'title': 'post 3', 'content': 'content of post 3'},
+#         {'id':4, 'title': 'post 4', 'content': 'content of post 4'},
+#     ]
+def index(request):
+    blog_title = "Latest Posts"
+
+    # getting data from post model
+    posts = Post.objects.all()
+
+    return render(request, 'blog/index.html', {'blog_title': blog_title, 'posts': posts})
+
+def detail(request, post_id):
+    # static data
+    # post = next((item for item in posts if item['id'] == int(post_id)), None)
+
+    # getting data from model by post id
+    post= Post.objects.get(pk=post_id)
+
+    # logger = logging.getLogger("TESTING")
+    # logger.debug(f'post variable is {post}')
+    return render(request, 'blog/detail.html', {'post': post})
+
+def old_url_redirect(request):
+    return redirect(reverse('blog:new_page_url'))
+
+def new_url_view(request):
+    return HttpResponse("This is the new URL")
